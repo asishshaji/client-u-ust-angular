1266b2a5da6f01829174b7b3b7edddf9fc1b68c7
Asish <asishshajithomas@gmail.com>
2021-05-30 11:56:09 +0530

    docker compose

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -1,2 +1,2 @@
-export const adminUrl = 'http://localhost:2020/api/v1/admin'
-export const employeeUrl = 'http://localhost:2020/api/v1/employee'
+export const adminUrl = 'http://localhost:2021/api/v1/admin'
+export const employeeUrl = 'http://localhost:2021/api/v1/employee'
